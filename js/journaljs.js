cb58ce0ff02a6f5fbb540fdcecb6ee28b26140a8
adi-gupta2 <98161394+adi-gupta2@users.noreply.github.com>
2023-02-05 13:05:33 +0530

    printing text extracted from backend

--- js/journaljs.js
+++ js/journaljs.js
@@ -61,7 +61,74 @@ window.addEventListener('load', () => {
 	});
 });
 
-// let water_tracker = document.getElementsByClassName(".new-water-icons")
+window.onload = function(read_diary){
+	const my_diary = read_diary;
+	const text_area = document.getElementById("new-diary-entry");
+	text_area.appendChild(my_diary);
+}
+
+window.onload = function(read_tasks){
+	const input = document.getElementById("new-task");
+	const list_el = document.querySelector("#tasks");
+	const my_tasks = Object.values(read_tasks);
+	console.log(read_tasks);
+	console.log(my_tasks);
+	for(var task of my_tasks){
+		const task_el = document.createElement('div');
+		task_el.classList.add('task');
+
+		const task_content_el = document.createElement('div');
+		task_content_el.classList.add('content');
+
+		task_el.appendChild(task_content_el);
+
+		const task_input_el = document.createElement('input');
+		task_input_el.classList.add('text');
+		task_input_el.type = 'text';
+		task_input_el.value = task;
+		task_input_el.setAttribute('readonly', 'readonly');
+
+		task_content_el.appendChild(task_input_el);
+
+		const task_actions_el = document.createElement('div');
+		task_actions_el.classList.add('actions');
+		
+		const task_edit_el = document.createElement('button');
+		task_edit_el.classList.add('edit');
+		task_edit_el.innerText = 'Edit';
+
+		const task_delete_el = document.createElement('button');
+		task_delete_el.classList.add('delete');
+		task_delete_el.innerText = 'Delete';
+
+		task_actions_el.appendChild(task_edit_el);
+		task_actions_el.appendChild(task_delete_el);
+
+		task_el.appendChild(task_actions_el);
+
+		list_el.appendChild(task_el);
+
+		input.value = '';
+
+		task_edit_el.addEventListener('click', (e) => {
+			if (task_edit_el.innerText.toLowerCase() == "edit") {
+				task_edit_el.innerText = "Save";
+				task_input_el.removeAttribute("readonly");
+				task_input_el.focus();
+			} else {
+				task_edit_el.innerText = "Edit";
+				task_input_el.setAttribute("readonly", "readonly");
+			}
+		});
+
+		task_delete_el.addEventListener('click', (e) => {
+			list_el.removeChild(task_el);
+		});
+	};
+}
+
+
+
 
 function add_water_icons(){
     const water_element = document.createElement("i");
